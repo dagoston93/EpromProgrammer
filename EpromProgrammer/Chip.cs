4da51de58769d7b8335c18632585dcfd022e911c
Agoston <dagoston93@gmail.com>
2021-07-10 19:17:36 +0200

    Chip class added.

---
+++ EpromProgrammer/Chip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpromProgrammer
+{
+    /**
+     * This class represents a supported chip
+     */ 
+    public struct Chip
+    {
+        public Chip(int pId, string pName, int pMemorySizeKb)
+        {
+            this.id = pId;
+            this.name = pName;
+            this.memorySizeKb = pMemorySizeKb;
+        }
+
+        public int id;
+        public string name;
+        public int memorySizeKb;
+    }
+}
--- EpromProgrammer/Form1.cs
+++ EpromProgrammer/Form1.cs
@@ -12,9 +12,22 @@ namespace EpromProgrammer
 {
     public partial class Form1 : Form
     {
+        /**
+         * Define the supported chips
+         */ 
+        private List<Chip> supportedChips= new List<Chip> {
+            new Chip(0, "TMS 27C0A10-12", 128)
+        };
+
+        /**
+         * The constructor
+         */ 
         public Form1()
         {
+            // Initialize the components
             InitializeComponent();
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
